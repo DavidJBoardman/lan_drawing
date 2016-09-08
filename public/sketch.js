aa9768c35a338242db9917f6a459b5addda2556a
Didlid <did.boardman@gmail.com>
2016-09-08 14:02:33 +0100

    Removed chat.js added chat functions to sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -22,6 +22,18 @@ function setup () {
 
   document.getElementById("brushSize").innerHTML = (ellipseXSize);
 
+  var socket = io();
+
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+
 
 }
 
